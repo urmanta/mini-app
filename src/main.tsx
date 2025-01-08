bd918c77bb8a75918dae6c0fe8317bec31ccc2e9
urmanta <urmanta@gmail.com>
2025-01-09 01:14:33 +0300

    Попытка закрепить футер

--- src/main.tsx
+++ src/main.tsx
@@ -10,6 +10,15 @@ const root = createRoot(container);
 window.Telegram.WebApp.ready();
 window.Telegram.WebApp.expand();
 
+if (window.visualViewport) {
+    window.visualViewport.addEventListener('resize', () => {
+        document.body.style.height = window.visualViewport?.height + 'px';
+    });
+}
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 0) window.scrollTo(0, 0);
+});
+
 root.render(
     <React.StrictMode>
         <App />
